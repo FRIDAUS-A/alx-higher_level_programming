a5c37601089c6ba6fc06caae8542542c3c4543a7
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-08 01:16:27 +0100

    Header

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,7 +10,7 @@
 */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *new2, *before, *tmp, *first;
+	listint_t *new, *before, *tmp, *first;
 	int count = 0;
 
 	first = *head;
