0f0fc9128d911f0475b52efd548297dbd8f44235
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-09 21:00:12 +0100

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,14 +7,14 @@
 */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list, *fast = list->next;
+	listint_t *slow = list, *fast = list;
 
-	while (slow != NULL && fast != NULL)
+	while (slow != NULL && fast->next != NULL)
 	{
+		slow = slow->next;
+		fast = (fast->next)->next;
 		if (slow == fast)
 			return (1);
-		slow = slow->next;
-		fast = fast->next;
 	}
 	return (0);
 }
