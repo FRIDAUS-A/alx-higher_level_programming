bac63d4466af31bf94051713d8d0e80e88d90695
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-10 23:09:23 +0100

    Dynamic Scrpt,

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 if (__name__ == "__main__"):
     from sys import argv
-    print("{} argument:".format(len(argv)))
+    if (len(argv) - 1 == 0):
+        str = "arguments."
+    elif (len(argv) - 1 == 1):
+        str = "argument:"
+    else:
+        str = "arguments:"
+    print("{} {}".format(len(argv) - 1, str))
     count = 1
     while (count < len(argv)):
         print("{:d}: {}".format(count, argv[count]))
