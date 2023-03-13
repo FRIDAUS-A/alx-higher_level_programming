a48d7f99010652525066ed40b77e4d0bc6a3861c
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-13 20:23:34 +0100

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for mem in matrix:
         for mem_1 in mem:
             print("{:d}".format(mem_1), end="")
-            if (mem_1 != mem[len(mem_1) - 1]):
+            if (mem_1 != mem[len(mem) - 1]):
                 print(" ", end="")
-            if (mem != matrix[len(mem) - 1]):
-                print()
+        print()
