3d348cc7c6be270ae431ce2781953a05473edf69
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-13 20:58:51 +0100

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     pos = 0
-    tuple_res = ()
+    tuple_a = list(tuple_a)
+    tuple_b = list(tuple_b)
+    tuple_res = []
     while (pos < 2):
         if (len(tuple_a) < 2):
             tuple_a.append(0)
@@ -9,4 +11,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b.append(0)
         tuple_res.append(tuple_a[pos] + tuple_b[pos])
         pos = pos + 1
+    tuple_res = tuple(tuple_res)
     return (tuple_res)
