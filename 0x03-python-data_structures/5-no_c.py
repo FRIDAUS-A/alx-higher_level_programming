c945009f48d4a6ed8e7b703c26eff40608c729ed
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-13 19:42:27 +0100

    removes all characters c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,17 +2,16 @@
 def no_c(my_string):
     count = 0
     my_string_tmp = ""
-    check = 1
     my_string = list(my_string)
     while (count < len(my_string) - 1):
         if (my_string[count] == 'c' or my_string[count] == 'C'):
-            if (check == 1):
+            if (my_string[count] == 'C'):
                 my_string[count] = ' '
             else:
                 my_string.remove(my_string[count])
-            check = check + 1
         count = count + 1
     for mem in my_string:
         my_string_tmp += mem
     my_string = my_string_tmp
     return (my_string)
+print(no_c("Best School"))
