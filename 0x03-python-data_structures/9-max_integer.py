5380af23e31cac1e426fcb63a6e043bbb93b8920
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-13 21:18:06 +0100

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    count = 0
+    if (len(my_list) == 0):
+        return (None)
+    test = my_list[0]
+    while (count < len(my_list)):
+        if (test < my_list[count]):
+            test = my_list[count]
+        count += count
+    return (test)
