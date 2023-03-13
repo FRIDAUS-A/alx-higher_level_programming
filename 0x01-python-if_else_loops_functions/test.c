bc9384d35d026b964a4250094b5fbfb9ef6c25f2
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-13 17:36:51 +0100

    print integers in a list

---
+++ 0x01-python-if_else_loops_functions/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char *arr = "";
+	/**
+	arr[0] = '1';
+	arr[1] = '2';
+	*/
+
+	printf("%s\n", arr);
+	return (0);
+}
---
+++ 0x02-python-import_modules/test.py
@@ -0,0 +1,4 @@
+import add_0
+tmp = dir(add_0)
+for ele in tmp:
+    print(ele)
