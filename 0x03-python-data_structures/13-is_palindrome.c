36c469faca02cc7056c051ce9334142750a8c1aa
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-15 11:06:55 +0100

    Check Palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: stores the address of the pointer to the first node
+ * Return: Allow success
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp = *head, *stop = NULL;
+
+	while ((*head)->next != stop)
+	{
+		while (tmp->next != stop)
+		{
+			tmp = tmp->next;
+		}
+		stop = tmp;
+		if ((*head)->n != stop->n)
+			return (0);
+		*head = (*head)->next;
+	}
+	return (1);
+}
