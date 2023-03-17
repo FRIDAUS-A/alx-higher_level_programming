878767558468a6e4af7b97beacb53e752a29d387
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-18 00:02:13 +0100

    Squared Simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    count, count_sub = 0, 0
-    while (count < len(matrix)):
-        new_matrix.append(matrix[count])
-        count += 1
     count = 0
-    while (count < len(new_matrix)):
-        while (count_sub < 3):
-            new_matrix[count][count_sub] = (matrix[count][count_sub]) ** 2
-            count_sub += 1
+    new_matrix = []
+    while (count < len(matrix)):
+        tmp_matrix = list(map(lambda x: x ** 2, matrix[count]))
         count += 1
+        new_matrix.append(tmp_matrix)
     return (new_matrix)
