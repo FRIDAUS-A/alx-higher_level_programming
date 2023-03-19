82085638b3129e5c9cd84ccb2ce31af64a37dc5e
FRIDAUS-A <fridausokoya@gmail.com>
2023-03-19 16:07:28 +0100

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if (my_list is None):
+        return (0)
+    total, freq = 0, 0
+    for mem in my_list:
+        total = mem[0] * mem[1] + total
+        freq = mem[1] + freq
+    return (total / freq)
