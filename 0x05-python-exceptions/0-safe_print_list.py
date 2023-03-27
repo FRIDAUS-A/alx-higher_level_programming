810fc4e57f207176bb7ebd418ec17dde1a4a0c5c
FRIDAUS <fridausokoya@gmail.com>
2023-03-27 15:29:52 +0100

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for ele in my_list:
         try:
-            print(ele, end="")
             if (count < x):
+                print(ele, end="")
                 count += 1
         except IndexError:
             pass
