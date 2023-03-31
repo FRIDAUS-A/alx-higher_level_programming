32fc4188c907f5406e66d52b411f07d0a9a86edd
FRIDAUS <fridausokoya@gmail.com>
2023-03-31 15:02:28 +0100

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,10 +12,6 @@ class Square:
         """
         self.__size = size
         self.__position = position
-        if (len(self.__position) is not 2 or type(self.__position) is not tuple
-                or type(self.__position[0]) is not int or
-                type(self.__position[1]) is not int):
-            raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
     def size(self):
