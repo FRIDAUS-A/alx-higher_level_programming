ccf222fdcec37feb2a65ae1aab20a28742ff7363
FRIDAUS <fridausokoya@gmail.com>
2023-04-16 12:09:47 +0100

    add two numbers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+"""add_integer module
+"""
 def add_integer(a, b=98):
     """A function that two numbers.
         Args:
                 a (int, float): first argument
                 b (int, float): second argument
-        >>> add_integer(1, 2)
-        3
-        >>> add_integer(100.3, -2)
-        98
     """
     if (type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
