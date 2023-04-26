f31fafdf71180e76472532d90526832b128ac17c
FRIDAUS <fridausokoya@gmail.com>
2023-04-26 12:03:53 +0100

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,11 +2,11 @@
 """WRITING A STRING TO THE END OF TEXTFILE"""
 
 
-def append_write(filename="", text=""):
+def write_file(filename="", text=""):
     """APPEND TO THE END OF A FILE.
         Args:
-            filename(string): name of file
-            text(string): text
+            filename (string): name of file
+            text (string): text
     """
     with open(filename, "w") as file:
         file.write(text)
