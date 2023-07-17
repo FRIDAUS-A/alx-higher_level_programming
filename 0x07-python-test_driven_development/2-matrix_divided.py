61e168dd25f3e1808cd25cc6186c187dde0f3dad
FRIDAUS <ayobamifridaus@gmail.com>
2023-07-17 11:37:42 +0100

    divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,30 +2,35 @@
 
 """A FUNCTION THAT DIVIDES ALL ELEMENTS OF A MATRIX"""
 
+
 def matrix_divided(matrix, div):
     """A function that divides a matrix by a value div
         Args:
             matrix (list[list]): maatrix
-            div (float or int): 
+            div (float or int):
     """
     if (type(matrix) is not list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
     else:
         for index_value in matrix:
             if (type(index_value) is not list):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a "
+                                "matrix (list of lists) of integers/floats")
             else:
                 for mem in index_value:
                     if (type(mem) != float and type(mem) != int):
-                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                        raise TypeError("matrix must "
+                                        "be a matrix (list of lists) "
+                                        "of integers/floats")
     for index_value in matrix:
-            if (len(matrix[1]) is not len(index_value)):
-                raise TypeError("Each row of the matrix must have the same size")
+        if (len(matrix[1]) is not len(index_value)):
+            raise TypeError("Each row of the "
+                            "matrix must have the same size")
     if (type(div) != float and type(div) != int):
         raise TypeError("div must be a number")
     if (div == 0):
         raise ZeroDivisionError("division by zero")
-    
     new_matrix = []
     for index_value in matrix:
         value = []
