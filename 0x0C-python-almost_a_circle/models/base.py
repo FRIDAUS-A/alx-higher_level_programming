6786869883da345e9b43340615895345be854d3e
FRIDAUS <ayobamifridaus@gmail.com>
2023-07-17 14:41:27 +0100

    Base of the class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ class Base:
     """Base class for other classes"""
     _nb_objects = 0
     def __init__(self, id=None):
+
         """constructor
             Args:
                 id (int): id for the class
