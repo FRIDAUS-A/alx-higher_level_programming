a105ed9880338fc45bc0209a7b8f9a9bae02b977
FRIDAUS <ayobamifridaus@gmail.com>
2023-07-19 17:23:03 +0100

    test for the base class

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -5,23 +5,23 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
-    def Test_unique(self):
+    def test_unique(self):
         """Test that each instance as a unique id"""
         obj1 = Base()
         obj2 = Base()
         self.assertNotEqual(obj1.id, obj2.id)
 
-    def Test_value(self):
+    def test_value(self):
         """Test that the id value is correctly assigned"""
         obj = Base(100)
         self.assertEqual(obj.id, 100)
 
-    def Test_idtype(self):
+    def test_idtype(self):
         """check if the id value ia an int"""
         obj = Base()
         self.assertIsInstance(obj.id, int)
 
-    def Test_private_instance_attribute(self):
+    def test_private_instance_attribute(self):
         """check if the __nb_objects is a private instance attribute"""
         with self.assertRaises(AttributeError):
             Base.__nb_objects
