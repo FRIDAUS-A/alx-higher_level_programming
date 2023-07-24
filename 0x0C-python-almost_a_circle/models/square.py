f94ae731c4822fdb807cfd8245a7dbb5849a6fc8
FRIDAUS <ayobamifridaus@gmail.com>
2023-07-24 07:04:17 +0100

    Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""The square class"""
+
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """The class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """defines the atrribute of the class insatnce
+        Args:
+            size (int): size of the square
+            x (int): position x of the square
+            y (int): position y of the square
+            id (int): object id
+        """
+        super().__init__(size, size, x, y, id)
+        self.__size = size
+        self.__width = size
+        self.__height = size
+        self.__x = x
+        self.__y = y
+
+    @property
+    def size(self):
+        """getter for size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if (isinstance(value, int) is False):
+            raise TypeError("width must be an integer")
+        else:
+            self.__size = value
+
+    def __str__(self):
+            """return thr string representation of an object"""
+            return f"[Square] ({self.id}) {self.__x}/{self.__y} - {self.__size}"
+
+    def update(self, *args, **kwargs):
+        if (len(args) != 0):
+            if (len(args) >= 1):
+                self.id = args[0]
+            if (len(args) >= 2):
+                self.__size = args[1]
+            if (len(args) >= 3):
+                self.__x = args[2]
+            if (len(args) >= 4):
+                self.__y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.__size = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Square"""
+        dict = {'id': self.id, 'x': self.__x, 'size': self.__size, 'y': self.__y}
+        return dict
