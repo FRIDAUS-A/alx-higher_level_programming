0783969e76a657799eb9bfef0e7c6527f2244365
FRIDAUS <ayobamifridaus@gmail.com>
2023-07-25 18:36:59 +0100

    Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,12 +33,7 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if (value <= 0):
             raise ValueError("width must be > 0")
-        self.__width = value
-        if (type(value) is not int):
-            raise TypeError("width must be an integer")
-        if (value <= 0):
-            raise ValueError("width must be > 0")
-        self.__height = value
+        self.__size = value
 
     def __str__(self):
         """return thr string representation of an object"""
