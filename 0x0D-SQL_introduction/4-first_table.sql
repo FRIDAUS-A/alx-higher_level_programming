1f8942276175fd8b8393d8582dd7f25f0fcc052a
FRUDAUS-A <ayobamifridaus@gmail.com>
2023-08-17 19:02:54 +0100

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+-- A script that creates a table called first_table
+-- in the current database in your MySQL server
+
+CREATE TABLE IF NOT EXISTS first_table;
+INSERT INTO first_table 
+(
+	id INT
+	name VARCHAR(256)
+)
