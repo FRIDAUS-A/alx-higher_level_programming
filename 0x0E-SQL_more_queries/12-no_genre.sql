832702eceba29c6362310e34d848b86589e1c513
FRUDAUS-A <ayobamifridaus@gmail.com>
2023-08-19 22:24:50 +0100

    No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- A that lists all shows contained in 
+-- hbtn_0d_tvshows that have at least one genre linked.
+
+SELECT t.title, tg.genre_id FROM tv_shows AS t 
+LEFT JOIN tv_show_genres AS tg 
+ON t.id = tg.show_id
+WHERE genre_id IS NULL
+ORDER BY t.title ASC, genre_id ASC;
