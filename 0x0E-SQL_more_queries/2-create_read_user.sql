e8ae1f809064eec859a73096fbefe47ea49c3046
FRUDAUS-A <ayobamifridaus@gmail.com>
2023-08-19 12:58:01 +0100

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- A script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+DROP DATABASE IF EXISTS hbtn_0d_2;
+CREATE DATABASE hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
