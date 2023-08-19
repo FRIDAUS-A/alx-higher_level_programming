ac7e56e1ca1dae1f504cc94c957f2a4d35629f96
FRUDAUS-A <ayobamifridaus@gmail.com>
2023-08-19 22:08:29 +0100

    Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- A that lists all shows contained in 
+-- hbtn_0d_tvshows that have at least one genre linked.
+
+SELECT t.title, tg.genre_id FROM tv_shows AS t 
+JOIN tv_show_genres as tg 
+ON t.id = tg.show_id ORDER BY t.title ASC, genre_id ASC;
