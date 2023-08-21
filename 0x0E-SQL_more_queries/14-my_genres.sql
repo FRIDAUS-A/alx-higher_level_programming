70cd29db329e1824dab2946497cc58c1194fc685
FRUDAUS-A <ayobamifridaus@gmail.com>
2023-08-21 09:05:27 +0100

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,7 @@
 -- A script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexters
 
-SELECT t.name FROM tv_genres AS t JOIN (
-	SELECT tg.genre_id FROM tv_show_genres AS tg
-	JOIN tv_shows AS t
-	ON tg.show_id = t.id
-	WHERE t.id = 8
-) AS g
-ON t.id = g.genre_id
+SELECT t.name FROM tv_genres AS t
+JOIN tv_show_genres as tg
+ON t.id = tg.genre_id
+WHERE tg.show_id = 8
 ORDER BY t.name ASC;
