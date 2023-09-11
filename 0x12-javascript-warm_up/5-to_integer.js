fca9ed307d7cfbfb0cdf43f798252c5655bb7df8
FRIDAUS <fridausokoya@gmail.com>
2023-09-11 17:39:58 +0100

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const firstArg = Number(process.argv[2]);
 if (isNaN(firstArg)) {
   console.log('Not a number');
 } else {
-  console.log(Math.floor(firstArg));
+  console.log('My number: ' + Math.floor(firstArg));
 }
