349a203fdd8374cd8bcf940ccf21d4839c4d749b
FRIDAUS <fridausokoya@gmail.com>
2023-09-11 18:28:18 +0100

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-
-for (let index = 0; index < process.argv[2]; index++) {
-  for (let Subindex = 0; Subindex < process.argv[2]; Subindex++) {
+const size = process.argv[2];
+if (isNaN(size)) {
+  console.log('Missing size');
+}
+for (let index = 0; index < size; index++) {
+  for (let Subindex = 0; Subindex < size; Subindex++) {
     process.stdout.write('X');
   }
   console.log();
