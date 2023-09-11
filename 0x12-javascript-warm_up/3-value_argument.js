6e7f94798e8d80ddf5067e23661d07371e0a7ea0
FRIDAUS <fridausokoya@gmail.com>
2023-09-11 16:56:56 +0100

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -5,8 +5,8 @@ const args = process.argv;
 while (args[count]) {
   count++;
 }
-if (count === 3) {
-  console.log(args[count - 1]);
+if (count >= 3) {
+  console.log(args[2]);
 } else if (count === 2) {
   console.log('No argument');
 }
