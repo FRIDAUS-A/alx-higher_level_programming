82c4b85045555d5c4029bf48cc4261d49d12dc01
FRIDAUS <fridausokoya@gmail.com>
2023-09-11 16:38:37 +0100

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
 const len = process.argv.length - 1;
-if (len > 1) {
+if (len === 2) {
   console.log('Argument found');
+} else if (len > 2) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
