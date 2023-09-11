c0248301c1e57ca941fdccace0d3715666c316f1
FRIDAUS <fridausokoya@gmail.com>
2023-09-11 18:08:51 +0100

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let index = 0; index < myArray.length; index++) {
   console.log(myArray[index]);
