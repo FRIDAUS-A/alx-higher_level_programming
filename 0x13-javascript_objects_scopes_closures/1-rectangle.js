18c7c9ad926852b1cc5df456bab56c2b28af2043
FRIDAUS <fridausokoya@gmail.com>
2023-09-12 12:37:10 +0100

    Rectangle #1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
- width;
- height;
-  constructor (width, height) {
-    this.width = width;
-    this.height = height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 };
