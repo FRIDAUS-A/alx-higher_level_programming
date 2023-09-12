7775f1424aed1f77586844273bae6e5a85fa6260
FRIDAUS <fridausokoya@gmail.com>
2023-09-12 17:45:34 +0100

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   let index = list.length - 1;
-  newList = []
+  const newList = [];
   while (index >= 0) {
     newList.push(list[index]);
     index--;
   }
   return (newList);
-}
+};
