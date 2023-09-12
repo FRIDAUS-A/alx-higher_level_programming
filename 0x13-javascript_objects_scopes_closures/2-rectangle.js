5cbc1112e5f6affc42e005f81f1fee537f4c0dc4
FRIDAUS <fridausokoya@gmail.com>
2023-09-12 12:59:25 +0100

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,9 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-  if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-  } else {
+    if (!(w <= 0 || h <= 0 || w === undefined || h === undefined)) {
       this.width = w;
       this.height = h;
-  }
+    }
   }
 };
