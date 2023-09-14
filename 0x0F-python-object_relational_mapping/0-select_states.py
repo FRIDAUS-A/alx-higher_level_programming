7d818d9725ff16c4367612f290407e3ae918cf6c
FRIDAUS <fridausokoya@gmail.com>
2023-09-14 15:51:00 +0100

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """A python script to get all the colums in a table"""
-import sys
 import MySQLdb
-db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-     password=sys.argv[2], database=sys.argv[3], port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-result = cur.fetchall()
-for value in result:
-    print(value)
+import sys
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+         password=sys.argv[2], database=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cur.fetchall()
+    for value in result:
+        print(value)
