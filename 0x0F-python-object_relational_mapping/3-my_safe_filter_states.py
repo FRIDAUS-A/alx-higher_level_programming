4c80cbd5554423163f9fc1e5ad22b5c8c9535929
FRIDAUS <fridausokoya@gmail.com>
2023-09-15 11:09:19 +0100

    SQL Injction...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""SQL Injection..."""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', sys.argv[1],
+                         sys.argv[2], sys.argv[3], 3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=BINARY '%s'\
+                ORDER BY id ASC" % (sys.argv[4]))
+    res = cur.fetchall()
+    for value in res:
+        print(value)
+    cur.close()
+    db.close()
