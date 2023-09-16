19a6ec1541f97c3190adff2007d0a2156d81dc79
FRIDAUS <fridausokoya@gmail.com>
2023-09-16 16:15:02 +0100

    relationship city clas

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a Python file similar to model_state.py named
+model_city.py that contains the class definition of a City.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from relationship_state import Base, State
+
+
+class City(Base):
+    """
+    class that defines each city
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
