da67d2a3badc375db9c14d8d50a045d6875f2b36
FRIDAUS <fridausokoya@gmail.com>
2023-09-30 13:15:00 +0100

    cuRL a json file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body.
-curl -s -X POST -H "Cotent-Type: application/json" -d "@$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
