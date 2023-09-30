391d20abe9647c4792ae421d24487be4a3789a08
FRIDAUS <fridausokoya@gmail.com>
2023-09-30 13:46:15 +0100

    Catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing
-curl -s -X PUT -H "Content-Type: application/json" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
