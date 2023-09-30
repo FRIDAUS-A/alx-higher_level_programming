8a0008ff87f46f63ddfe6cf7636df597c69a8958
FRIDAUS <fridausokoya@gmail.com>
2023-09-30 10:44:12 +0100

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -X GET -H "HTTP Status Code: 200 OK" "$1"
+curl -s -L "$1"
