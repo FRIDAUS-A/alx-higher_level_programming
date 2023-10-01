805401817f7ccf11c0a13145e01cfff29990f9ff
FRIDAUS <fridausokoya@gmail.com>
2023-10-01 09:59:53 +0100

    What is my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(str(html)[2:4]))
+    print("\t- utf8 content: {}".format(html.decode()))
