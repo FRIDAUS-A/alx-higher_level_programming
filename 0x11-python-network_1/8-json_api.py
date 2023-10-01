f777d657bbf31953907c5cf92bb0b825c0a096bc
FRIDAUS <fridausokoya@gmail.com>
2023-10-01 15:15:42 +0100

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     else:
         data['q'] = sys.argv[1]
     html = requests.post("http://0.0.0.0:5000/search_user", data=data)
-    json = html.json()
     try:
+        json = html.json()
         if json == {}:
             print("No result")
         else:
-            print(f"[{json['id']}]  {json['name']}")
+            print(f"[{json['id']}] {json['name']}")
     except ValueError:
         print("Not a valid JSON")
