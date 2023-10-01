1943c8a678fa9c623d74ac2bfed08c5cdb5bedf4
FRIDAUS <fridausokoya@gmail.com>
2023-10-01 11:22:15 +0100

    Post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,7 @@ a Python script that takes in a URL and an email, sends
 a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
-import urrlib.request
+import urllib.request
 import urllib.parse
 import sys
 if __name__ == '__main__':
