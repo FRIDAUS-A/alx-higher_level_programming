1fa75bf5eb2176454e45de415470afaa0519a9a1
FRIDAUS <fridausokoya@gmail.com>
2023-10-01 16:46:14 +0100

    Interview Solution

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Holberton Interview Question
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/" + sys.argv[2] + "/" +\
+                                            sys.argv[1] + "/commits"
+    print(url)
+    body = requests.get(url)
+    json = body.json()
+    count = 0
+    for mem in json:
+        count += 1
+        print(f"{mem.get('sha')}:"
+              f"{mem.get('commit').get('author').get('name')}")
+        if count == 10:
+            break
